53436123b6413cf85835697cdc3fe9b0c491802a
Azurba <joaoh.pimenta.giudice@gmail.com>
2022-03-19 10:06:11 -0600

    add home prototype; add images; fixes in build.gradle, pubspec.yaml and androidManifest

--- lib/Tabs/home_tab.dart
+++ lib/Tabs/home_tab.dart
@@ -1,30 +1,26 @@
-import 'dart:ui';
+// ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
+// ignore: use_key_in_widget_constructors
 class HomeTab extends StatelessWidget {
-  const HomeTab({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
     Widget _buildBack() => Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.blue,
-            Colors.white
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
-      ),
-    );
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.white],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+        );
     return Stack(
       children: [
         _buildBack(),
-        const CustomScrollView(
-          slivers: [
+        CustomScrollView(
+          slivers: <Widget>[
             SliverAppBar(
               floating: true,
               snap: true,
@@ -34,6 +30,39 @@ class HomeTab extends StatelessWidget {
                 title: Text("Welcome"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              future: FirebaseFirestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: Container()),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data!.docs.map((doc) {
+                      return StaggeredTile.count(
+                          doc.data()['x'], doc.data()['y']);
+                    }).toList(),
+                    children: snapshot.data!.docs.map((docs) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: docs.data()['image'],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
+                  );
+                }
+              },
             )
           ],
         )
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:king_oopers_app/screens/home_screen.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(const MyApp());
+  debugPaintSizeEnabled = false;
+  runApp(const MaterialApp(
+    title: "King Oopers",
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'King Oopers',
-      theme: ThemeData(primarySwatch: Colors.blue, primaryColor: Colors.white),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
-    );
+    Widget _background() => Container(
+          //background
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.white],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+        );
+
+    //ADD navigateTo to arguments
+    // ignore: non_constant_identifier_names
+    Widget _HomeButtons(String text, color) => Padding(
+          padding: const EdgeInsets.fromLTRB(1, 0, 1, 20),
+          child: Container(
+            width: 215,
+            child: ElevatedButton(
+                child: Text(
+                  text,
+                  style: TextStyle(fontSize: 29.0, color: color),
+                ),
+                onPressed: () {
+                  //Navigator.push(context, MaterialPageRoute(builder: (context) => PlayerPanel()));
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(255, 255, 204, 1))),
+          ),
+        );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "King Oopers",
+            style: TextStyle(color: Colors.black, fontSize: 35),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+        body: Stack(
+          children: [
+            _background(),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Container(
+                      height: MediaQuery.of(context).size.height / 10,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/kingOopers.PNG')),
+                      )),
+                ),
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _HomeButtons("Dairy", Colors.red),
+                        _HomeButtons("Meat", Colors.red),
+                        _HomeButtons("Cereal", Colors.red),
+                        _HomeButtons("Fruits", Colors.red),
+                      ],
+                    ))
+              ],
+            )
+          ],
+        ));
   }
 }
