d47dd69c243e60d2631ac5abd55ac42af8fe39a6
Azurba <joaoh.pimenta.giudice@gmail.com>
2022-03-20 11:58:18 -0600

    add products to dairy page

--- lib/Off/Dairy.dart
+++ lib/Off/Dairy.dart
@@ -39,9 +39,14 @@ class _Dairy extends State<DairyState> {
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          cardsWidget('assets/images/milk.png', 'Milk'),
-          cardsWidget('assets/images/milk.png', 'Milk'),
-          cardsWidget('assets/images/milk.png', 'Milk'),
+          cardsWidget('assets/images/milk.png', 'Borden - 2% milk'),
+          cardsWidget('assets/images/LacotoseFree.PNG', 'Fa!rlife - Lactose Free Milk'),
+          cardsWidget('assets/images/chedarCheese.PNG', 'Kroger - Mild Cheddar Cheese - Block'),
+          cardsWidget('assets/images/ProvoloneCheese.PNG', 'Sargento - Provolone Cheese - Slices'),
+          cardsWidget('assets/images/sourCream.PNG', 'Daisy - Sour Cream'),
+          cardsWidget('assets/images/blueYogurt.PNG', 'Chobani - Greek Yogurt - Blueberry'),
+          cardsWidget('assets/images/strawYogurt.PNG', 'Chobani - Greek Yogurt - Strawberry'),
+
         ],
       ),
     );
