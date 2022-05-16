b6163ff6faed0adf62dc2993cba32800356fe8cd
Azurba <joaoh.pimenta.giudice@gmail.com>
2022-05-16 10:06:23 -0600

    add product object

---
+++ lib/GroceryProduct.dart
@@ -0,0 +1,10 @@
+class GroceryProduct{
+  const GroceryProduct({
+    required this.price,
+    required this.name,
+    required this.image,
+});
+  final double price;
+  final String name;
+  final String image;
+}
